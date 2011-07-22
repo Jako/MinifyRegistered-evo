2993e80deb3c8ddcba80c591af1161ea13328f86
Thomas Jakobi <thomas.jakobi@partout.info>
2011-07-22 11:46:33 +0200

    Update to 0.1.1

--- minifyRegistered.plugin.php
+++ minifyRegistered.plugin.php
@@ -1,112 +1,121 @@
-<?php 
+<?php
 /**
  * minifyRegistered
  *
  * @category 	plugin
- * @version 	0.1
+ * @version 	0.1.1
  * @license 	http://www.gnu.org/copyleft/gpl.html GNU Public License (GPL)
  * @author		Jako (thomas.jakobi@partout.info)
  *
- * @internal    @description: <strong>0.1</strong> collect the registered javascripts and css files and minify them by minify (http://code.google.com/p/minify/)
- * @internal    @plugin code: include(MODX_BASE_PATH.'assets/plugins/minifyregistered/minifyRegistered.plugin.php');
+ * @internal    @description: <strong>0.1.1</strong> collect the registered
+ *              javascripts and css files and minify them by minify
+ *              (http://code.google.com/p/minify/)
+ * @internal    @plugin code:
+ *              include(MODX_BASE_PATH.'assets/plugins/minifyregistered/minifyRegistered.plugin.php');
  * @internal	@events: OnLoadWebDocument, OnWebPagePrerender
- *
- * @internal    The Plugin needs a working installation of http://code.google.com/p/minify/ in the folder /min in the webroot.
+ * @internal	@configuration: &groupJs=Group minified files in 'assets/js':;list;yes,no;yes
+ * 
+ * @internal    The Plugin needs a working installation of
+ *              http://code.google.com/p/minify/ in the folder /min in the
+ *              webroot.
  */
 
+$groupJs = (isset($groupJs)) ? (bool) $groupJs : true; 
  
-if (MODX_BASE_PATH == '') {
-    die('<h1>ERROR:</h1><p>Please use the MODx Content Manager instead of accessing this file directly.</p>');
+if(MODX_BASE_PATH == '') {
+	die('<h1>ERROR:</h1><p>Please use the MODx Content Manager instead of accessing this file directly.</p>');
 }
 
 $e = &$modx->Event;
 switch ($e->name) {
-    case 'OnLoadWebDocument': {
-            // Mask closing head and body tags - so MODX can't insert the registered blocks during 'OnParseDocument'
-            $modx->documentContent = preg_replace("/(<\/head>)/i", '##/HEAD##', $modx->documentContent);
-            $modx->documentContent = preg_replace("/(<\/body>)/i", '##/BODY##', $modx->documentContent);
-            break;
-        }
-    case 'OnWebPagePrerender': {
-            include (MODX_BASE_PATH.'assets/plugins/minifyregistered/includes/chunkie.class.inc.php');
-            if (!function_exists('getTVDisplayFormat')) {
-                include (MODX_BASE_PATH.'manager/includes/tmplvars.format.inc.php');
-            }
-            
-            $registeredScripts = array();
-            // Are there any scripts loaded by $modx->regClient... ?
-            if (count($modx->loadedjscripts)) {
-                // collect the registered blocks and assign them to the right document part
-                foreach ($modx->loadedjscripts as $scriptSrc=>$scriptParam) {
-                    $startup = ($scriptParam['startup']) ? 'head' : 'body';
-                    if (stripos($scriptSrc, '<') === FALSE) {
-                        if (substr(trim($scriptSrc), -3) == '.js') {
-                            if (trim(dirname(trim($scriptSrc)), '/') == 'assets/js') {
-                                $registeredScripts[$startup.'_jsminjs'][$scriptParam['pos']] = str_replace('assets/js', '', $scriptSrc);
-                            } else {
-                                $registeredScripts[$startup.'_jsmin'][$scriptParam['pos']] = $scriptSrc;
-                            }
-                        } elseif (substr(trim($scriptSrc), -4) == '.css') {
-                            $registeredScripts['head_cssmin'][$scriptParam['pos']] = $scriptSrc;
-                        } else {
-                            $registeredScripts[$startup][$scriptParam['pos']] = $scriptSrc;
-                        }
-                    } else {
-                        $registeredScripts[$startup][$scriptParam['pos']] = $scriptSrc;
-                    }
-                }
-                
-                // prepare the output of the registered blocks
-                $headScripts = $bodyScripts = '';
-                if (count($registeredScripts['head_jsminjs'])) {
-                    $headScripts .= '<script src="/min/?b=assets/js&amp;f='.implode(',', $registeredScripts['head_jsminjs']).'" type="text/javascript"></script>'."\r\n";
-                }
-                if (count($registeredScripts['head_jsmin'])) {
-                    $headScripts .= '<script src="/min/?f='.implode(',', $registeredScripts['head_jsmin']).'" type="text/javascript"></script>'."\r\n";
-                }
-                if (count($registeredScripts['head_cssmin'])) {
-                    $headScripts .= '<link href="/min/?f='.implode(',', $registeredScripts['head_cssmin']).'" rel="stylesheet" type="text/css" />'."\r\n";
-                }
-                if (count($registeredScripts['head'])) {
-                    $headScripts .= "\r\n".implode("\r\n", $registeredScripts['head']);
-                }
-                if (count($registeredScripts['body_jsmin'])) {
-                    $bodyScripts .= '<script src="/min/?f='.implode(',', $registeredScripts['body_jsmin']).'" type="text/javascript"></script>'."\r\n";
-                }
-                if (count($registeredScripts['body'])) {
-                    $bodyScripts .= "\r\n".implode("\r\n", $registeredScripts['body']);
-                }
-                
-                // parse the output of the registered blocks
-                $tmpDocumentObject = array();
-                if (is_array($modx->placeholders)) {
-                    // add placeholder to the temporary documentObject
-                    $tmpDocumentObject = $modx->placeholders;
-                }
-                foreach ($modx->documentObject as $key=>$value) {
-                    // check for template variables
-                    if (is_array($value)) {
-                        $tmpDocumentObject[$key] = getTVDisplayFormat($value[0], $value[1], $value[2], $value[3], $value[4], $modx->documentObject['id']);
-                    } else {
-                        $tmpDocumentObject[$key] = $value;
-                    }
-                }
-                
-                $parser = new minifyChunkie('@CODE:'.$headScripts);
-                foreach ($tmpDocumentObject as $key=>$value) {
-                    $parser->AddVar($key, $value);
-                }
-                $headScripts = $parser->Render()."\r\n";
-                
-                $parser = new minifyChunkie('@CODE:'.$bodyScripts);
-                foreach ($tmpDocumentObject as $key=>$value) {
-                    $parser->AddVar($key, $value);
-                }
-                $bodyScripts = $parser->Render()."\r\n";
-            }
-            // insert the scripts at the end of the head and body
-            $modx->documentOutput = str_replace('##/HEAD##', $headScripts.'</head>', $modx->documentOutput);
-            $modx->documentOutput = str_replace('##/BODY##', $bodyScripts.'</body>', $modx->documentOutput);
-        }
+	case 'OnLoadWebDocument' : {
+		// Mask closing head and body tags - so MODX can't insert the registered blocks
+		// during 'OnParseDocument'
+		$modx->documentContent = preg_replace("/(<\/head>)/i", '##/HEAD##', $modx->documentContent);
+		$modx->documentContent = preg_replace("/(<\/body>)/i", '##/BODY##', $modx->documentContent);
+		break;
+	}
+	case 'OnWebPagePrerender' : {
+		include (MODX_BASE_PATH.'assets/plugins/minifyregistered/includes/chunkie.class.inc.php');
+		if(!function_exists('getTVDisplayFormat')) {
+			include (MODX_BASE_PATH.'manager/includes/tmplvars.format.inc.php');
+			include (MODX_BASE_PATH.'manager/includes/tmplvars.commands.inc.php');
+		}
+
+		$registeredScripts = array();
+		// Are there any scripts loaded by $modx->regClient... ?
+		if(count($modx->loadedjscripts)) {
+			// collect the registered blocks and assign them to the right document part
+			foreach($modx->loadedjscripts as $scriptSrc => $scriptParam) {
+				$startup = ($scriptParam['startup']) ? 'head' : 'body';
+				if(strpos($scriptSrc, '<') === FALSE) {
+					if(substr(trim($scriptSrc), -3) == '.js') {
+						if($groupJs && (trim(dirname(trim($scriptSrc)), '/') == 'assets/js')) {
+							$registeredScripts[$startup.'_jsminjs'][$scriptParam['pos']] = str_replace('assets/js', '', $scriptSrc);
+						} else {
+							$registeredScripts[$startup.'_jsmin'][$scriptParam['pos']] = $scriptSrc;
+						}
+					} elseif(substr(trim($scriptSrc), -4) == '.css') {
+						$registeredScripts['head_cssmin'][$scriptParam['pos']] = $scriptSrc;
+					} else {
+						$registeredScripts[$startup][$scriptParam['pos']] = $scriptSrc;
+					}
+				} else {
+					$registeredScripts[$startup][$scriptParam['pos']] = $scriptSrc;
+				}
+			}
+
+			// prepare the output of the registered blocks
+			$headScripts = $bodyScripts = '';
+			if(count($registeredScripts['head_jsminjs'])) {
+				$headScripts .= '<script src="/min/?b=assets/js&amp;f='.implode(',', $registeredScripts['head_jsminjs']).'" type="text/javascript"></script>'."\r\n";
+			}
+			if(count($registeredScripts['head_jsmin'])) {
+				$headScripts .= '<script src="/min/?f='.implode(',', $registeredScripts['head_jsmin']).'" type="text/javascript"></script>'."\r\n";
+			}
+			if(count($registeredScripts['head_cssmin'])) {
+				$headScripts .= '<link href="/min/?f='.implode(',', $registeredScripts['head_cssmin']).'" rel="stylesheet" type="text/css" />'."\r\n";
+			}
+			if(count($registeredScripts['head'])) {
+				$headScripts .= "\r\n".implode("\r\n", $registeredScripts['head']);
+			}
+			if(count($registeredScripts['body_jsmin'])) {
+				$bodyScripts .= '<script src="/min/?f='.implode(',', $registeredScripts['body_jsmin']).'" type="text/javascript"></script>'."\r\n";
+			}
+			if(count($registeredScripts['body'])) {
+				$bodyScripts .= "\r\n".implode("\r\n", $registeredScripts['body']);
+			}
+
+			// parse the output of the registered blocks
+			$tmpDocumentObject = array();
+			if(is_array($modx->placeholders)) {
+				// add placeholder to the temporary documentObject
+				$tmpDocumentObject = $modx->placeholders;
+			}
+			foreach($modx->documentObject as $key => $value) {
+				// check for template variables
+				if(is_array($value)) {
+					$tmpDocumentObject[$key] = getTVDisplayFormat($value[0], $value[1], $value[2], $value[3], $value[4], $modx->documentObject['id']);
+				} else {
+					$tmpDocumentObject[$key] = $value;
+				}
+			}
+
+			$parser = new minifyChunkie('@CODE:'.$headScripts);
+			foreach($tmpDocumentObject as $key => $value) {
+				$parser->AddVar($key, $value);
+			}
+			$headScripts = $parser->Render()."\r\n";
+
+			$parser = new minifyChunkie('@CODE:'.$bodyScripts);
+			foreach($tmpDocumentObject as $key => $value) {
+				$parser->AddVar($key, $value);
+			}
+			$bodyScripts = $parser->Render()."\r\n";
+		}
+		// insert the scripts at the end of the head and body
+		$modx->documentOutput = str_replace('##/HEAD##', $headScripts.'</head>', $modx->documentOutput);
+		$modx->documentOutput = str_replace('##/BODY##', $bodyScripts.'</body>', $modx->documentOutput);
+	}
 }
 ?>
